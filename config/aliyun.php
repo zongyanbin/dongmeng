fa4b6a2ad0e680c8c82570b62af30849a46190c5
ps照 <315635152@qq.com>
2020-12-23 20:22:03 +0800

    删除配置

--- config/aliyun.php
+++ config/aliyun.php
@@ -1,8 +1,7 @@
 <?php
 return [
     'oss'=>[
-         'accessKeyId'=>'LTAI4GES4CHPCbfXmu9aRMBT',
-         'accessKeySecret'=>'Cv9FsWRrQA0ABl9lqCjOtvBJCYHX66'
+
     ],
     'onDemand'=>['callback'=>'test']
 ];
